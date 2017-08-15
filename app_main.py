ea480e945689580feb1df7017be4d07016fa05c5
muchen2 <muchen2@illinois.edu>
2017-08-15 11:40:57 +0800

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,3 +1,19 @@
+"""
+   Copyright 2017 Mu Chen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 import os
 from os.path import join
 import json
@@ -192,7 +208,11 @@ def main_page ():
 		valence_sad_ratio = 0,
 		best_cand = "Not Determined",
 		sec_best_cand = "Not Determined")
-
+"""
+Send waveform pyplot image to the webpage after
+the analysis has been done. The image file is not
+saved and is sent through a StringIO object instead.
+"""
 @app.route ("/waveform_image")
 def get_waveform_img ():
 	global imgIO
